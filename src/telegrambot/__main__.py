1b531cff182cbeb97ba62b504c8711df07b9e4d0
Daniel Ibarrola <dis96051@gmail.com>
2023-08-18 13:31:48 -0600

    CLI parser

--- src/telegrambot/__main__.py
+++ src/telegrambot/__main__.py
@@ -1,4 +1,19 @@
+from telegrambot.cli import create_parser
 
 
 def main():
-    return "Hello World"
+    parser = create_parser()
+    args = parser.parse_args()
+
+    if args.mode == "message":
+        send_message(args)
+    elif args.mode == "photo":
+        send_photo(args)
+    elif args.mode == "updates":
+        get_updates(args)
+    else:
+        raise ValueError(f"Invalid mode {args.mode}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/unit/test_cli.py
@@ -0,0 +1,12 @@
+
+
+def test_send_message():
+    pass
+
+
+def test_send_photo():
+    pass
+
+
+def test_get_updates():
+    pass
